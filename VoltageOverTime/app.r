7a5f7fbab370bd6f1dfcd5445fb574ff03213cdb
Graham Home <grahamhome333@gmail.com>
2016-05-24 19:08:17 -0400

    VoltageOverTime complete-ish
    VoltageOverTime app complete for now; needs to be revisited later to
    figure out how to update graph without flashing display.

--- VoltageOverTime/app.r
+++ VoltageOverTime/app.r
@@ -1,4 +1,4 @@
- #A Shiny app which uses ggvis to display real-time statistical analysis of PMU data from a power grid.
+ #A Shiny app which uses Plotly to display real-time statistical analysis of PMU data from a power grid.
 library(shiny)
 library(plotly)
 
@@ -7,7 +7,7 @@ source(file="read_data.r")
 #UI (webpage) is defined here - inputs and outputs
 ui <- fluidPage(
 
-  titlePanel("Changes in Bus Voltage Over Time"),
+  titlePanel("Changes in Bus Frequency Over Time"),
 
   plotlyOutput("plot"),
 
@@ -16,16 +16,32 @@ ui <- fluidPage(
   fluidRow(
   	column(12,
   		sliderInput("time", "Time range to examine",  min = 1, max = 845, value = c(500, 750)),
-  		radioButtons("speed", "Animation speed", c("Half speed"=0.5, "Normal speed"=1, "Double speed"=2), inline=TRUE),
-  		actionButton("start", "Go!")
+  		radioButtons("speed", "Animation speed", c("Half speed"=0.5, "Normal speed"=1, "Double speed"=2), selected=1, inline=TRUE),
+  		actionButton("go", "Go!")
   	)
   )
 )
 
 #R functionality is defined here
 server <- function(input, output) {
+
+  #Create a new list of maps whenever the Go button is clicked
+  maps <- eventReactive(input$go, {
+    mapList <- list()
+    for (t in input$time[[1]]:input$time[[2]]) {
+      mapList[[(t+1)-input$time[[1]]]] <- get_plotlyfreqmap(t)
+    }
+    print(mapList) #It doesn't work without this, but I have no idea why.
+  })
+
+  #Index for map list
+  i <- 0
+
+  #Render a plot by selecting a map from the list, updating the index every 10 ms.
 	output$plot <- renderPlotly({
-		get_plotlyfreqmap(input$time[[1]])
+    invalidateLater(10)
+    i <- (i+1)
+		maps()[[i]]
 		})
 
 }
--- VoltageOverTime/read_data.r
+++ VoltageOverTime/read_data.r
@@ -7,6 +7,28 @@ library(scales)
 library(reshape2)
 library(plotly)
 
+#Change the frequency column of bus_locs with the frequencies for a given time 
+# must (assign function return to bus_locs)
+update_freq <- function(time){
+  tf <- t(F[time,-1])
+  tf <- cbind(rownames(tf),tf)
+  colnames(tf) <- c("Bus.Name","Frequency")
+  bus_locs <- merge(subset(bus_locs,select = c("Bus.Num","Bus.Name","Sub.Name","Latitude","Longitude","Voltage")),tf, by="Bus.Name")
+  bus_locs$Frequency <- as.numeric(as.character(bus_locs$Frequency))
+  bus_locs
+}
+#Change the voltage column of bus_locs with the frequencies for a given time
+# must (assign function return to bus_locs)
+update_volt <- function(time){
+  vf <- t(V[time,-1])
+  vf <- cbind(rownames(vf),vf)
+  colnames(vf) <- c("Bus.Name","Voltage")
+  bus_locs <- merge(subset(bus_locs,select = c("Bus.Num","Bus.Name","Sub.Name","Latitude","Longitude","Frequency")),vf, by="Bus.Name")
+  bus_locs$Voltage <- as.numeric(as.character(bus_locs$Voltage))
+  bus_locs
+} 
+
+
 #get covariance matrix of the frequency values, starting from whatever the previous time (curr_sf)
 # was to whatever "time" is. 
 get_covbus_freq <- function(time) {
@@ -49,6 +71,104 @@ get_covbus_volt <- function(time) {
   assign("curr_sv",time,envir = .GlobalEnv)
   assign("Sv",Sv,envir = .GlobalEnv)
 }
+
+#init_environ <- function(){
+buses <- read.csv("Data/tenn150_buses.csv")
+substat <- read.csv("Data/tenn150_substations.csv")
+loads <- read.csv("Data/tenn150_loads.csv")
+generators <- read.csv("Data/tenn150_generators.csv")
+linesb <- read.csv("Data/tenn150_lines.csv")
+trans <- read.csv("Data/tenn150_transformers.csv")
+V <- read.csv("Data/tenn150gmd_bus_voltage.csv")
+F <- read.csv("Data/tenn150gmd_bus_frequency.csv")
+#V <- read.csv("tenn150ts_bus_voltage.csv")
+#F <- read.csv("tenn150ts_bus_frequency.csv")
+
+bn <- buses$Bus.Name
+bn <- gsub(" ",".",bn)
+buses$Bus.Name <- bn
+
+bn <- linesb$To.Bus.Name
+bn <- gsub(" ",".",bn)
+linesb$To.Bus.Name <- bn
+bn <- linesb$From.Bus.Name
+bn <- gsub(" ",".",bn)
+linesb$From.Bus.Name <- bn
+
+sub_buses <- merge(buses,substat,by = "Sub.ID")
+
+#replace names in Frequency table with just bus names 
+cn <- colnames(F[,-1])
+cn <- gsub("Bus[.]","",cn)
+cn <- gsub("[.]Frequency","",cn)
+cn <- gsub("[.][.]",".",cn)
+cn <- gsub("Gallatin[.]TN[.]","Gallatin.(TN)",cn)
+colnames(F) <- c("Time",cn)
+#replace names in Voltage table with just bus names 
+cn <- colnames(V[,-1])
+cn <- gsub("Bus[.]","",cn)
+cn <- gsub("[.]V[.]pu","",cn)
+cn <- gsub("[.][.]",".",cn)
+cn <- gsub("Gallatin[.]TN[.]","Gallatin.(TN)",cn)
+colnames(V) <- c("Time",cn)
+
+#Set up dataframe with each bus name, bus number, the name of the substation it's at,
+# the lat/long of the station, the frequency (at time=0), and the voltage (at time=0)
+bus_locs <- data.frame(sub_buses$Bus.Num,sub_buses$Bus.Name,sub_buses$Sub.Name.x,sub_buses$Latitude,sub_buses$Longitude,"Frequency","Voltage")
+colnames(bus_locs) <- c("Bus.Num","Bus.Name","Sub.Name", "Latitude","Longitude","Frequency","Voltage")
+bus_locs$Frequency <- bus_locs$Voltage <- 0 
+
+#Add the frequency values from time t=0 to bus_locs dataframe
+bus_locs <- update_freq(1)
+#Add the voltage values from time t=0 to bus_locs dataframe
+bus_locs <- update_volt(1)
+
+#Add the from/to bus latitudes/longitudes to the line data
+linesb <- merge(linesb,bus_locs[,c("Bus.Num","Latitude","Longitude")], by.x = "From.Bus.Num", by.y = "Bus.Num")
+linesb <- merge(linesb,bus_locs[,c("Bus.Num","Latitude","Longitude")], by.x = "To.Bus.Num", by.y = "Bus.Num")
+cn <- colnames(linesb)
+cn <- gsub("Latitude[.]x","From.Latitude",cn)
+cn <- gsub("Longitude[.]x","From.Longitude",cn)
+cn <- gsub("Latitude[.]y","To.Latitude",cn)
+cn <- gsub("Longitude[.]y","To.Longitude",cn)
+colnames(linesb) <- cn
+linesb$Variance <- 0
+linesb$Line.id <- seq_len(nrow(linesb))
+#trans <- merge(trans,bus_locs[,c("Bus.Num","Latitude","Longitude")], by.x = "From.Bus.Num", by.y = "Bus.Num")
+#trans <- merge(trans,bus_locs[,c("Bus.Num","Latitude","Longitude")], by.x = "To.Bus.Num", by.y = "Bus.Num")
+#Create the map to use as the background for the ggplot
+mapten <- get_map(location = c(lon = mean(bus_locs$Longitude), lat = mean(bus_locs$Latitude)), 
+                zoom = 6, maptype = "roadmap", scale = 2)
+g <- ggmap(mapten)+ scale_x_continuous(limits = c(-90.6, -81), expand = c(0, 0)) + scale_y_continuous(limits = c(34.5, 37), expand = c(0, 0))
+#Set up the variables for the voltage covariance matrix
+Xv <- as.matrix(V[,-1])
+Sv <- cov(Xv[1:2,])
+curr_sv <- 3
+xvbar <- colMeans(Xv[1:2,])
+#Set up these variables for the frequency covariance matrix
+Xf <- as.matrix(F[,-1])
+Sf <- cov(Xf[1:2,])
+curr_sf <- 3
+xfbar <- colMeans(Xf[1:2,])
+
+#This finds the min/max covariances over the whole dataset, for labeling purposes
+mincovf <- 1
+maxcovf <- 0
+for (t in 3:845) {
+  get_covbus_freq(t)
+  mincovf <- ifelse(min(Sf[,])<mincovf,min(Sf[,]),mincovf)
+  maxcovf <- ifelse(max(Sf[,])>maxcovf,max(Sf[,]),maxcovf)
+}
+mincovv <- 1
+maxcovv <- 0
+for (t in 3:845) {
+  get_covbus_volt(t)
+  mincovv <- ifelse(min(Sv[,])<mincovv,min(Sv[,]),mincovv)
+  maxcovv <- ifelse(max(Sv[,])>maxcovv,max(Sv[,]),maxcovv)
+}
+
+
+
 #covariance matrix of the voltage values
 #get_covbus_volt <- function(time,S) {
 #  X <- as.matrix(V[,-1])
@@ -123,27 +243,6 @@ plotmapfreq <- function(t){
   g
 }
 
-#Change the frequency column of bus_locs with the frequencies for a given time 
-# must (assign function return to bus_locs)
-update_freq <- function(time){
-  tf <- t(F[time,-1])
-  tf <- cbind(rownames(tf),tf)
-  colnames(tf) <- c("Bus.Name","Frequency")
-  bus_locs <- merge(subset(bus_locs,select = c("Bus.Num","Bus.Name","Sub.Name","Latitude","Longitude","Voltage")),tf, by="Bus.Name")
-  bus_locs$Frequency <- as.numeric(as.character(bus_locs$Frequency))
-  bus_locs
-}
-#Change the voltage column of bus_locs with the frequencies for a given time
-# must (assign function return to bus_locs)
-update_volt <- function(time){
-  vf <- t(V[time,-1])
-  vf <- cbind(rownames(vf),vf)
-  colnames(vf) <- c("Bus.Name","Voltage")
-  bus_locs <- merge(subset(bus_locs,select = c("Bus.Num","Bus.Name","Sub.Name","Latitude","Longitude","Frequency")),vf, by="Bus.Name")
-  bus_locs$Voltage <- as.numeric(as.character(bus_locs$Voltage))
-  bus_locs
-} 
-
 get_plotlyvoltmap <- function(t){
   bus_locs <- update_volt(t)
   linesb <- update_busline_voltcov(t)
@@ -200,100 +299,6 @@ get_plotlyfreqmap <- function(t){
   p
 }
 
-#init_environ <- function(){
-buses <- read.csv("Data/tenn150_buses.csv")
-substat <- read.csv("Data/tenn150_substations.csv")
-loads <- read.csv("Data/tenn150_loads.csv")
-generators <- read.csv("Data/tenn150_generators.csv")
-linesb <- read.csv("Data/tenn150_lines.csv")
-trans <- read.csv("Data/tenn150_transformers.csv")
-V <- read.csv("Data/tenn150gmd_bus_voltage.csv")
-F <- read.csv("Data/tenn150gmd_bus_frequency.csv")
-#V <- read.csv("tenn150ts_bus_voltage.csv")
-#F <- read.csv("tenn150ts_bus_frequency.csv")
-
-bn <- buses$Bus.Name
-bn <- gsub(" ",".",bn)
-buses$Bus.Name <- bn
-
-bn <- linesb$To.Bus.Name
-bn <- gsub(" ",".",bn)
-linesb$To.Bus.Name <- bn
-bn <- linesb$From.Bus.Name
-bn <- gsub(" ",".",bn)
-linesb$From.Bus.Name <- bn
-
-sub_buses <- merge(buses,substat,by = "Sub.ID")
-
-#replace names in Frequency table with just bus names 
-cn <- colnames(F[,-1])
-cn <- gsub("Bus[.]","",cn)
-cn <- gsub("[.]Frequency","",cn)
-cn <- gsub("[.][.]",".",cn)
-cn <- gsub("Gallatin[.]TN[.]","Gallatin.(TN)",cn)
-colnames(F) <- c("Time",cn)
-#replace names in Voltage table with just bus names 
-cn <- colnames(V[,-1])
-cn <- gsub("Bus[.]","",cn)
-cn <- gsub("[.]V[.]pu","",cn)
-cn <- gsub("[.][.]",".",cn)
-cn <- gsub("Gallatin[.]TN[.]","Gallatin.(TN)",cn)
-colnames(V) <- c("Time",cn)
-
-#Set up dataframe with each bus name, bus number, the name of the substation it's at,
-# the lat/long of the station, the frequency (at time=0), and the voltage (at time=0)
-bus_locs <- data.frame(sub_buses$Bus.Num,sub_buses$Bus.Name,sub_buses$Sub.Name.x,sub_buses$Latitude,sub_buses$Longitude,"Frequency","Voltage")
-colnames(bus_locs) <- c("Bus.Num","Bus.Name","Sub.Name", "Latitude","Longitude","Frequency","Voltage")
-bus_locs$Frequency <- bus_locs$Voltage <- 0 
-
-#Add the frequency values from time t=0 to bus_locs dataframe
-bus_locs <- update_freq(1)
-#Add the voltage values from time t=0 to bus_locs dataframe
-bus_locs <- update_volt(1)
-
-#Add the from/to bus latitudes/longitudes to the line data
-linesb <- merge(linesb,bus_locs[,c("Bus.Num","Latitude","Longitude")], by.x = "From.Bus.Num", by.y = "Bus.Num")
-linesb <- merge(linesb,bus_locs[,c("Bus.Num","Latitude","Longitude")], by.x = "To.Bus.Num", by.y = "Bus.Num")
-cn <- colnames(linesb)
-cn <- gsub("Latitude[.]x","From.Latitude",cn)
-cn <- gsub("Longitude[.]x","From.Longitude",cn)
-cn <- gsub("Latitude[.]y","To.Latitude",cn)
-cn <- gsub("Longitude[.]y","To.Longitude",cn)
-colnames(linesb) <- cn
-linesb$Variance <- 0
-linesb$Line.id <- seq_len(nrow(linesb))
-#trans <- merge(trans,bus_locs[,c("Bus.Num","Latitude","Longitude")], by.x = "From.Bus.Num", by.y = "Bus.Num")
-#trans <- merge(trans,bus_locs[,c("Bus.Num","Latitude","Longitude")], by.x = "To.Bus.Num", by.y = "Bus.Num")
-#Create the map to use as the background for the ggplot
-#mapten <- get_map(location = c(lon = mean(bus_locs$Longitude), lat = mean(bus_locs$Latitude)), 
-  #                zoom = 6, maptype = "roadmap", scale = 2)
-#g <- ggmap(mapten)+ scale_x_continuous(limits = c(-90.6, -81), expand = c(0, 0)) + scale_y_continuous(limits = c(34.5, 37), expand = c(0, 0))
-#Set up the variables for the voltage covariance matrix
-Xv <- as.matrix(V[,-1])
-Sv <- cov(Xv[1:2,])
-curr_sv <- 3
-xvbar <- colMeans(Xv[1:2,])
-#Set up these variables for the frequency covariance matrix
-Xf <- as.matrix(F[,-1])
-Sf <- cov(Xf[1:2,])
-curr_sf <- 3
-xfbar <- colMeans(Xf[1:2,])
-
-#This finds the min/max covariances over the whole dataset, for labeling purposes
-mincovf <- 1
-maxcovf <- 0
-for (t in 3:845) {
-  get_covbus_freq(t)
-  mincovf <- ifelse(min(Sf[,])<mincovf,min(Sf[,]),mincovf)
-  maxcovf <- ifelse(max(Sf[,])>maxcovf,max(Sf[,]),maxcovf)
-}
-mincovv <- 1
-maxcovv <- 0
-for (t in 3:845) {
-  get_covbus_volt(t)
-  mincovv <- ifelse(min(Sv[,])<mincovv,min(Sv[,]),mincovv)
-  maxcovv <- ifelse(max(Sv[,])>maxcovv,max(Sv[,]),maxcovv)
-}
 
 findvolt_greatest_outlier_station <- function(time){
   curr_volts <- as.matrix(t(V[time,-1]))
@@ -325,7 +330,6 @@ create_voltvideo <- function(start,stop,vidtitle){
     }
   },video.name = vidtitle)
 }
-
 
 #plot the a line graph of each bus from {start} to {stop} of the voltage
 plotvoltage <- function(start,stop,plotname){
