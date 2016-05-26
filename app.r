554460a92c620d2697c5d4fb28c6e505beecfa25
Graham Home <grahamhome333@gmail.com>
2016-05-26 16:16:53 -0400

    Data and Plot Imports Added
    Added ability to select and import dataset and plot type from available
    files.

--- app.r
+++ app.r
@@ -1,12 +1,148 @@
+#Main app of PowerViewer application.
+#Author: Graham Home <grahamhome333@gmail.com>
+
+#Dependencies
 library(shiny)
-ui <- fluidPage(
+library(shinythemes)
 
-  titlePanel("Welcome to Power Viewer!"),
-  #TODO: Add intro text and OK button to launch choose_data.r
-)
+#Create reactive values for user's progress
+progress <- reactiveValues()
+progress$stage <- 1
 
-#R functionality is defined here
-server <- function(input, output) {
+#Reactive values for user's chosen files
+files <- reactiveValues()
+
+#Data file selection utility function - returns filenames of all data import files.
+getDataFileNames <- function() {
+	print("getting file names")
+	files <- list.files(path="data/", pattern="*.r")
 }
 
-shinyApp(ui=ui, server=server)
+#Returns the names of the plot files compatible with the dataset selected by the user
+getCompatiblePlots <- function() {
+	eval(parse(text=gsub(".r", "_plots()", files$data)))
+}
+
+#App UI is defined here - inputs & outputs
+ui <- fluidPage(
+	theme=shinytheme("spacelab"),	#Space is cool
+
+  	absolutePanel(top="25%", height="50%", left="30%", width="40%", fixed=TRUE, style="background-color:#e6e6e6",
+  		uiOutput("content")
+  	)
+)
+
+#R functionality is defined here - how inputs affect outputs
+server <- function(input, output, session) {
+
+	#Content window changes state based on user activity
+	output$content <- renderUI({
+
+		#Intro screen
+		if (progress$stage == 1) {
+			fluidRow(
+  				column(8, offset=2,
+  					h1("Welcome to Power Viewer!", style="text-align:center")
+  				),
+  				column(12,
+  					h3("Power Viewer is a tool for viewing power grid data with a library of plotting methods.
+  						The next two screens will allow you to choose from the available data sets and plot styles.
+  						Ready to begin?", style="line-height:150%; text-align:center")
+  				),
+  				column(4, offset=4,
+  				actionButton("start", "Start", width="100%", style="margin-top:10%")
+  		 		),
+  		 		column(8, offset=2,
+  		 			uiOutput("dataPicker")
+  		 		)
+  			)
+
+  		# Dataset picker
+		} else if (progress$stage == 2) {
+			fluidRow(
+				column(2,
+  					actionLink("back", "Previous", icon=icon("arrow-left"))
+  				),
+  				column(8,
+  					h1("Choose Data Set", style="text-align:center"),
+  					br(),
+  					selectInput("dataset", "Data Set", getDataFileNames()),
+  					actionButton("selectData", "Import Data")
+  				)
+  			)
+
+  		#Plot type picker	
+		} else if (progress$stage == 3) {
+			fluidRow(
+				column(2,
+  					actionLink("back", "Previous", icon=icon("arrow-left"))
+  				),
+  				column(8, offset=2,
+  					h1("Choose Plot Type", style="text-align:center"),
+  					br(),
+  					selectInput("plottype", "Plot Type", getCompatiblePlots()),
+  					actionButton("selectPlot", "Select Plot")
+  				)
+  			)
+
+  		#Plot display
+		} else if (progress$stage == 4) {
+			div(
+				fluidRow(
+					column(2,
+  						actionLink("back", "Previous", icon=icon("arrow-left"))
+  					),
+  					column(10,
+  						imageOutput("plot", height="600px", width="1500px")
+  					)
+  				),
+  				fluidRow(
+  					column(12,
+  						sliderInput("time", "Time range to examine",  min = 1, max = 845, value = c(500, 750), width = "75%"),
+  						radioButtons("speed", "Animation speed", c("Very slow"=10, "Normal speed"=1, "Very fast"=0.01), selected=1, inline=TRUE),
+  						actionButton("go", "Go!")
+  					)
+  				)
+  			)
+		}
+	})
+
+	#When "Start" button is clicked
+	observeEvent(input$start, {
+		#Switch to dataset picker activity
+		progress$stage <- 2
+	})
+
+	#When "Import Data" button is clicked
+	observeEvent(input$selectData, {
+		#Switch to plot picker activity.
+		progress$stage <- 3
+		#Import dataset chosen by user
+		source(file=paste("data/", input$dataset, sep=""))
+		eval(parse(text=gsub(".r", "()", input$dataset)))
+		#Store name of data import file
+		files$data <- input$dataset
+
+	})
+
+	#When "Select Plot" button is clicked
+	observeEvent(input$selectPlot, {
+		#Switch to display activity
+		progress$stage <- 4
+		#Import plot function set chosen by user
+		source(file=paste("plots/", input$plottype, sep=""))
+		#Store name of plot function file
+		files$plot <- input$plottype
+	})
+
+	#When the "Previous" button is clicked
+	observeEvent(input$back, {
+		if (progress$stage > 0) {	#Just in case I forget and put it on the home page by mistake
+			progress$stage <- progress$stage-1
+		}
+	})
+}
+
+#Start the app
+mainApp <- shinyApp(ui=ui, server=server)
+runApp(mainApp, port=5678)
--- plots/README.txt
+++ plots/README.txt
@@ -2,3 +2,10 @@ This is the directory for plot generation functions.
 Each R file in this directory must contain a function that
 shares its name with the file itself. This function must return
 a plot object.
+
+linker.txt contains import and display links for plot generation methods.
+Links specify which import method and which display method to use
+with a given plot generation method.
+Links must be written one per line in the following format:
+plot_file_name:input_file_name_1,input_file_name_2:display_file_name
+There must be a blank line at the end of the file (hit Enter after entering last link).
