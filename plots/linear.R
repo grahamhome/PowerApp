e7772635b7d9c9250cb1650d38ece5dba04230be
Jacob Haut <jakehaut@gmail.com>
2016-06-08 15:18:41 -0400

    updated linear plot so the legend is a bit better (still not perfect); updated the correlation to have a better looking title (also still not great); starting to do work on clustering; updated thumbnails

--- plots/linear.R
+++ plots/linear.R
@@ -1,4 +1,4 @@
-library(reshape)
+
 fnames <- function(){
   n <- list(Linear="linear",
             Voltage="plot_voltage",
@@ -13,7 +13,7 @@ plot_voltage <- function(start,stop){
   #  geom_line() +
   #  theme(legend.position="right")
   #p
-  par(mar=c(5.1, 4.1, 4.1, 8.1), xpd=TRUE)
+  par(mar=c(5.1, 4.1, 4.1, 14.1), xpd=TRUE)
   #start <- ifelse(missing(start),1,start)
   #stop <- ifelse(missing(stop),nrow(Volt),stop)
   xrange <- range(Volt[start:stop,1])
@@ -29,17 +29,17 @@ plot_voltage <- function(start,stop){
     bus <- Volt[start:stop,z]
     lines(y=bus,x=Volt[start:stop,1],col=colors[z],type = "l",lty=linetype[z])
   }
-  legend("topright", inset=c(-0.2,-0.15),legend = 1:n,col = colors, lty=linetype,cex=0.8)
+  legend("topright", inset=c(-0.2,-0.15),legend = colnames(Volt[,-1]),col = colors, lty=linetype,cex=0.8)
 }
 #plot the a line graph of each bus from {start} to {stop} of the voltage
 plot_frequency <- function(start,stop){
-  par(mar=c(5.1, 4.1, 4.1, 8.1), xpd=TRUE)
+  par(mar=c(5.1, 4.1, 4.1, 14.1), xpd=TRUE)
   #start <- ifelse(missing(start),1,start)
   #stop <- ifelse(missing(stop),nrow(Freq),stop)
   xrange <- range(Freq[start:stop,1])
   yrange <- range(Freq[start:stop,-1])
   plot(xrange,yrange,type = "n",xlab = "Time (seconds)",ylab = "Frequency",
-       main=paste("Frequency at time",start,"to",stop,sep = " "))
+       main=paste("Frequency at time",Freq[start,1],"to",Freq[stop,1],sep = " "))
   num_sig_bus <- 0
   n <- ncol(Freq)-1
   colors <- rainbow(n)
@@ -49,5 +49,8 @@ plot_frequency <- function(start,stop){
     bus <- Freq[start:stop,z]
     lines(y=bus,x=Freq[start:stop,1],col=colors[z],type = "l",lty=linetype[z])
   }
-  legend("topright", inset=c(-0.2,-0.15),legend = 1:n,col = colors, lty=linetype,cex=0.8)
+  legend("topright", inset=c(-.30,-0.15),legend = colnames(Freq[,-1]),col = colors, lty=linetype,cex=0.8)
 }
+
+
+
